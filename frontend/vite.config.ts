2899be6fa19af9097828509f4d75047a655371d6
Yuriy Bondaryuk <ybondaryuk@marfa-tech.com>
2025-05-03 10:18:44 +0300

    first commit

---
+++ frontend/vite.config.ts
@@ -0,0 +1,33 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    vueDevTools(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    },
+  },
+  server: {
+    port: 5002,
+    proxy: {
+      // Проксируем запросы к API
+      '/api': {
+        target: 'http://localhost:5001',
+      },
+      // Проксируем WebSocket соединения
+      '/socket.io': {
+        target: 'ws://localhost:5001',
+        ws: true,
+        rewriteWsOrigin: true,
+      },
+    },
+  },
+})
