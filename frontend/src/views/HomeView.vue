677aa2f567afaad4ed32e348d89f2a188af0b408
YurokBo <anonymous@example.invalid>
2025-05-14 22:22:04 +0300

    step-2: add pages

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,9 +1,13 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+<template>
+  <h1>Hello, home</h1>
+</template>
+
+<script lang="ts">
+
 </script>
 
-<template>
-  <main>
-    <TheWelcome />
-  </main>
-</template>
+<style lang="scss">
+h1 {
+  color: red;
+}
+</style>
